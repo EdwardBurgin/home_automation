7e8444d31518cbe018fe24edb17817b32ebc660c
Burgin, Edward <edward.burgin@accenture.com>
2019-01-03 01:28:49 +0000

    fix

--- radiator.py
+++ radiator.py
@@ -4,14 +4,14 @@
 # In[39]:
 
 
-f= open("temp.txt","w+")
+f = open("temp.txt","w+")
 f.close()
 
 
 # In[41]:
 
 
-f= open("temp.txt","a+")
+f = open("temp.txt","a+")
 val = 3
 f.write('%d\n'%val)
 f.close()
@@ -77,40 +77,40 @@ print(os.getcwd())
 temp_sensor = 'w1_slave'
 
 def temp_raw():
-	f=open(temp_sensor,'r')
-	lines = f.readlines()
-	f.close()
-	return lines
+    f=open(temp_sensor,'r')
+    lines = f.readlines()
+    f.close()
+    return lines
 
 def read_temp():
-	lines=temp_raw()
-	while lines[0].strip()[-3:]!='YES':
-		time.sleep(0.2)
-		lines = temp_raw()
-	temp_output = lines[1].find('t=')
-	if temp_output != -1:
-		temp_string = lines[1].strip()[temp_output+2:]
-		temp_c = float(temp_string)/1000.0
-		return temp_c
+    lines=temp_raw()
+    while lines[0].strip()[-3:]!='YES':
+        time.sleep(0.2)
+        lines = temp_raw()
+    temp_output = lines[1].find('t=')
+    if temp_output != -1:
+        temp_string = lines[1].strip()[temp_output+2:]
+        temp_c = float(temp_string)/1000.0
+        return temp_c
 
 while True:
-	curr = read_temp()
-    f= open("temp.txt","a+")
+    curr = read_temp()
+    f = open("temp.txt","a+")
     f.write('%d\n'%curr)
     f.close()
     if curr < 17:
         print 'turning on radiator'
         GPIO.output (11, True)
-		GPIO.output (15, True)
-		GPIO.output (16, True)
-		GPIO.output (13, True)
-		# let it settle, encoder requires this
-		time.sleep(0.1)
-		# Enable the modulator
-		GPIO.output (22, True)
-		# keep enabled for a period
-		time.sleep(0.25)
-		# Disable the modulator
-		GPIO.output (22, False)
-	time.sleep(20)
+        GPIO.output (15, True)
+        GPIO.output (16, True)
+        GPIO.output (13, True)
+        # let it settle, encoder requires this
+        time.sleep(0.1)
+        # Enable the modulator
+        GPIO.output (22, True)
+        # keep enabled for a period
+        time.sleep(0.25)
+        # Disable the modulator
+        GPIO.output (22, False)
+    time.sleep(20)
 
