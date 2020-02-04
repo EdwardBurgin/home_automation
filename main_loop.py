b3c26a61ff3fd4ee7fd7424d404146712a32c40d
Edward Burgin <edster3203@googlemail.com>
2020-02-04 11:08:03 +0000

    Update main_loop.py
    removed plotting to function, fix mem attempt

--- main_loop.py
+++ main_loop.py
@@ -4,12 +4,8 @@ from ipywidgets import interact, interactive, fixed, interact_manual
 from IPython.display import display, Image
 
 import gc
-import matplotlib
-matplotlib.use('Agg')
-import pandas as pd
-import matplotlib.pyplot as plt
 
-import seaborn as sns
+import pandas as pd
 import collections
 import datetime
 import numpy as np
@@ -29,7 +25,40 @@ message_log = collections.deque(maxlen=200)
 temp_ini()
 wireless_ini()
 
-
+def plot_fn(d):
+    '''
+    Place plotting in limited scope.
+    '''
+#     import matplotlib
+#     matplotlib.use('Agg')
+#     import seaborn as sns
+#     import matplotlib.pyplot as plt
+    from matplotlib.pyplot import savefig, plot, close, clf, cla, subplots, xticks
+    
+#         fig=plt.figure()
+#         ax = fig.add
+#         
+#         plt.figure()
+#         plt.plot()
+    fig, ax= subplots(figsize=(12,3))
+    ax.plot(d)
+#         sns.lineplot(data = d, style = 'event', ci = 'sd', err_style='band', ax=ax)
+    _ = xticks(rotation = 30)
+    savefig('graph.png')
+    close('all')
+#         plt.show()a
+#         plt.show(block=False)
+#     plt.close(fig)
+#     plt.close('all')
+#     plt.close()
+#     plt.clf()
+#     plt.cla()
+    del ax
+    del fig
+    del d
+    gc.collect()
+    
+    
 def radiator_controller(temp_lower, time_finish, interval_load):
     """
     Reads temperature, appends to limited width buffer which is writen to a txt file.
@@ -39,7 +68,7 @@ def radiator_controller(temp_lower, time_finish, interval_load):
     while True:
         file_log = open('log.txt','w')
         t = datetime.datetime.now()
-        tot_m, used_m, free_m = map(int, os.popen('free -t -m').readlines()[-1].split()[1:])
+        tot_m, used_m, free_m = map(int, os.popen('free -t -m').readlines()[-1].split()[1:])  #linux only
         curr = read_temp()
         temps.append((t, curr))
         if (curr <= temp_lower) & (t.hour < time_finish):
@@ -69,31 +98,12 @@ def radiator_controller(temp_lower, time_finish, interval_load):
         file_log.close()
 #         plt.plot([i[1] for i in temps])
         d = pd.Series(index = [i[0] for i in temps], data = [i[1] for i in temps])#.plot(figsize=(20,7))
-#         fig=plt.figure()
-#         ax = fig.add
-#         
-#         plt.figure()
-#         plt.plot()
-        fig, ax=plt.subplots(figsize=(12,3))
-        ax.plot(d)
-#         sns.lineplot(data = d, style = 'event', ci = 'sd', err_style='band', ax=ax)
-        _ = plt.xticks(rotation = 30)
-        plt.savefig('graph.png')
-#         plt.show()
-#         plt.show(block=False)
-        plt.close(fig)
-        plt.close('all')
-        plt.close()
-        plt.clf()
-        plt.cla()
+
         if t.hour > time_finish:
             time.sleep(interval_load*4)
         else:
             time.sleep(interval_load)
 #         %reset
-        del ax
-        del fig
-        del d
-        gc.collect()
+
 print('hi, starting loop')        
 radiator_controller(temp_lower=16.7, time_finish=8, interval_load=180)
